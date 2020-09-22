06f169bf7ec4ce8e011b15cc8269a1194e128dae
AaratiS12 <aarati.srikumar@gmail.com>
2020-09-22 15:53:45 +0000

    Add necessary heroku files

--- user_tweets.py
+++ user_tweets.py
@@ -11,10 +11,10 @@ dotenv_path = join(dirname(__file__), 'tweepy.env')
 load_dotenv(dotenv_path)
 
 
-consumer_key = os.getenv("KEY")
-consumer_secret = os.getenv("KEY_SECRET")
-access_token = os.getenv("TOKEN")
-access_token_secret = os.getenv("TOKEN_SECRET")
+consumer_key = os.environ['consumer_key']
+consumer_secret = os.environ['consumer_secret']
+access_token = os.environ['access_token']
+access_token_secret = os.environ['access_token_secret']
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
